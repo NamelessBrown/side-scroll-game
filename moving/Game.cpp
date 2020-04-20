52923e634231632643618c44014768a669884482
Nameless <tyrebrown873@gmail.com>
2020-04-20 15:58:38 -0400

    Added a scrolling background!

--- moving/Game.cpp
+++ moving/Game.cpp
@@ -1,14 +1,20 @@
 #include "Game.h"
 
 Game::Game(sf::RenderWindow& window)
-	:m_window(window), m_event(sf::Event())
+	:m_window(window), m_event(sf::Event()), m_deltatime(60.f), m_backgroundScrollSpeed(1.2f), m_backgroundScroll(0)
 {
+	m_backgroundTexture.loadFromFile("Textures/gameBack.jpg");
+	m_backgroundTexture.setSmooth(true);
+
+	m_backgroundSprite.setTexture(m_backgroundTexture);
+	m_backgroundSprite.setPosition(0.f, 0.f);
 }
 
 void Game::Run()
 {
 	while (m_window.isOpen())
 	{
+		m_deltatime = m_clock.restart().asSeconds() * 60.f;
 		Update();
 		Render();
 	}
@@ -27,14 +33,23 @@ void Game::PollEvent()
 	}
 }
 
+void Game::ChangeBackground()
+{
+	m_backgroundScroll = std::fmod((m_backgroundScroll + m_backgroundScrollSpeed * m_deltatime), 150);
+	m_backgroundSprite.setPosition((float)-m_backgroundScroll, 0.f);
+}
+
 void Game::Update()
 {
 	PollEvent();
+	ChangeBackground();
 }
 
 void Game::Render()
 {
 	m_window.clear();
+
+	m_window.draw(m_backgroundSprite);
 
 	m_window.display();
 }
--- moving/Game.h
+++ moving/Game.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 class Game
@@ -11,9 +12,16 @@ public:
 private:
 	sf::RenderWindow& m_window;
 	sf::Event m_event;
+	sf::Texture m_backgroundTexture;
+	sf::Sprite m_backgroundSprite;
+	sf::Clock m_clock;
 
+	float m_deltatime;
+	double m_backgroundScroll;
+	double m_backgroundScrollSpeed;
 
 	void PollEvent();
+	void ChangeBackground();
 	void Update();
 	void Render();
 };
