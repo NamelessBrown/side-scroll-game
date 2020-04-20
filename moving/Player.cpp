b75de525db6c284ffac27379436852586904aeef
Nameless <tyrebrown873@gmail.com>
2020-04-20 16:45:20 -0400

    Movement function

--- moving/Player.cpp
+++ moving/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 
 Player::Player()
+	:m_speed(4.5f)
 {
 	m_counter = 4;
 	m_textureRect = sf::IntRect(0, 0, 368 / m_counter - 50, 200 / m_counter + 1);
@@ -12,9 +13,36 @@ Player::Player()
 
 void Player::Update(float dt)
 {
+
 }
 
 void Player::Render(sf::RenderWindow& window)
 {
 	window.draw(m_sprite);
+}
+
+void Player::Movement(float dt, sf::Event& event)
+{
+	switch (event.type)
+	{
+	case sf::Event::KeyPressed:
+
+		switch (event.key.code)
+		{
+		case sf::Keyboard::Key::D:
+			m_sprite.move(m_speed * dt, 0.f);
+			break;
+		case sf::Keyboard::Key::A:
+			m_sprite.move(-m_speed * dt, 0.f);
+			break;
+		case sf::Keyboard::Key::W:
+			m_sprite.move(0.f, -m_speed * dt);
+			break;
+		default:
+			m_sprite.move(0.f, 0.f);
+			break;
+		}
+
+		break;
+	}
 }
