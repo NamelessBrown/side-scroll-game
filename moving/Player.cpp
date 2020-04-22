dad7cbdf6eed62cdbefea799c58c46e6cf93aaf0
Nameless <tyrebrown873@gmail.com>
2020-04-22 16:48:17 -0400

    ANIMATION!!!

--- moving/Player.cpp
+++ moving/Player.cpp
@@ -6,7 +6,11 @@ Player::Player()
 	m_counter = 0;
 	m_texture.loadFromFile("Textures/playerSheet.png");
 
-	m_animation = Animation(m_texture, 8, 46, 50, 46, 50);
+
+	m_animations[int(AnimationIndex::WalkingRight)] = Animation(m_texture, 3, 46 * 3, 50 * 3, 46, 50);
+	m_animations[int(AnimationIndex::WalkingLeft)] = Animation(m_texture, 3, 46 * 3, 50 * 3, 46, 50);
+	m_animations[int(AnimationIndex::Fighting)] = Animation(m_texture, 8, 0, 0, 46, 50);
+	m_animations[int(AnimationIndex::Standing)] = Animation(m_texture, 1, 0, 0, 46, 50);
 
 	m_sprite.setTexture(m_texture);
 	m_sprite.setPosition(0.f, 540.f);
@@ -15,8 +19,18 @@ Player::Player()
 void Player::Update(float dt)
 {
 	Movement(dt);
-	m_animation.Update(dt);
-	m_animation.Apply(m_sprite);
+
+	if (m_currentAnimation == AnimationIndex::WalkingLeft)
+	{
+		m_sprite.setScale(-1.0f, 1.0f);
+	}
+	else
+	{
+		m_sprite.setScale(1.0f, 1.0f);
+	}
+
+	m_animations[int(m_currentAnimation)].Update(dt);
+	m_animations[int(m_currentAnimation)].Apply(m_sprite);
 }
 
 void Player::Render(sf::RenderWindow& window)
@@ -29,17 +43,21 @@ void Player::Movement(float dt)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 	{
 		m_sprite.move(m_speed * dt, 0.f);
+		m_currentAnimation = AnimationIndex::WalkingRight;
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
 	{
 		m_sprite.move(-m_speed * dt, 0.f);
+		m_currentAnimation = AnimationIndex::WalkingLeft;
 	}
 	else
 	{
+		m_currentAnimation = AnimationIndex::Standing;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LShift))
 	{
+		m_currentAnimation = AnimationIndex::Fighting;
 	}
 
 }
--- moving/Player.h
+++ moving/Player.h
@@ -11,13 +11,20 @@ public:
 	void Update(float dt);
 	void Render(sf::RenderWindow& window);
 private:
+	enum class AnimationIndex
+	{
+		WalkingRight, Fighting, Standing, WalkingLeft, Count
+	};
+
+private:
 	sf::Texture m_texture;
 	sf::Sprite m_sprite;
 
 	int m_counter; //Used for the sprite sheet
 	float m_speed;
 
-	Animation m_animation;
+	Animation m_animations[int(AnimationIndex::Count)];
+	AnimationIndex m_currentAnimation = AnimationIndex::WalkingRight;
 
 	void Movement(float dt);
 };
