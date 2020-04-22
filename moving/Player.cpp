56464024dba1e35fe66796cb4039211e7500c5b5
Nameless <tyrebrown873@gmail.com>
2020-04-22 15:02:53 -0400

    Years... upon years trying to solve *this sprite sheet! Needs more Work!

--- moving/Player.cpp
+++ moving/Player.cpp
@@ -3,17 +3,17 @@
 Player::Player()
 	:m_speed(4.5f)
 {
-	m_counter = 4;
-	m_textureRect = sf::IntRect(0, 0, 368 / m_counter - 50, 200 / m_counter + 1);
-	m_texture.loadFromFile("Textures/playerSheet.png", m_textureRect);
+	m_counter = 0;
+	m_texture.loadFromFile("Textures/playerSheet.png");
 
 	m_sprite.setTexture(m_texture);
+	m_sprite.setTextureRect(sf::IntRect(0, 0, 46, 50));
 	m_sprite.setPosition(0.f, 540.f);
 }
 
 void Player::Update(float dt)
 {
-
+	Movement(dt);
 }
 
 void Player::Render(sf::RenderWindow& window)
@@ -21,28 +21,26 @@ void Player::Render(sf::RenderWindow& window)
 	window.draw(m_sprite);
 }
 
-void Player::Movement(float dt, sf::Event& event)
+void Player::Movement(float dt)
 {
-	switch (event.type)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
 	{
-	case sf::Event::KeyPressed:
-
-		switch (event.key.code)
-		{
-		case sf::Keyboard::Key::D:
-			m_sprite.move(m_speed * dt, 0.f);
-			break;
-		case sf::Keyboard::Key::A:
-			m_sprite.move(-m_speed * dt, 0.f);
-			break;
-		case sf::Keyboard::Key::W:
-			m_sprite.move(0.f, -m_speed * dt);
-			break;
-		default:
-			m_sprite.move(0.f, 0.f);
-			break;
-		}
-
-		break;
+		m_sprite.move(m_speed * dt, 0.f);
+		m_sprite.setTextureRect(sf::IntRect(0, 0, 46, 50));
 	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+	{
+		m_sprite.move(-m_speed * dt, 0.f);
+		m_sprite.setTextureRect(sf::IntRect(0, 0, 46, 50));
+	}
+	else
+	{
+		m_sprite.setTextureRect(sf::IntRect(0, 0, 46, 50));
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LShift))
+	{
+		m_sprite.setTextureRect(sf::IntRect(0, 0, 46, 50));
+	}
+
 }
--- moving/Player.h
+++ moving/Player.h
@@ -8,14 +8,14 @@ public:
 	Player();
 
 	void Update(float dt);
-	void Movement(float dt, sf::Event& event);
 	void Render(sf::RenderWindow& window);
 private:
 	sf::Texture m_texture;
-	sf::IntRect m_textureRect;
 	sf::Sprite m_sprite;
 
 	int m_counter; //Used for the sprite sheet
 	float m_speed;
+
+	void Movement(float dt);
 };
 
