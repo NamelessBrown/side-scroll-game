577518ea7805bbfe974d76a3962a5f88c0b52019
Nameless <tyrebrown873@gmail.com>
2020-04-23 13:34:31 -0400

    Test Zombie skinny

--- moving/Enemy.cpp
+++ moving/Enemy.cpp
@@ -10,7 +10,7 @@ Enemy::Enemy()
 	std::uniform_int_distribution<int> m_distribution(0, 4);
 
 	//m_type = (ZomebieType)m_distribution(rng);
-	m_type = ZomebieType::fat;
+	m_type = ZomebieType::skinny;
 
 	switch (m_type)
 	{
@@ -19,6 +19,9 @@ Enemy::Enemy()
 		break;
 	case ZomebieType::fat:
 		m_animation = Animation(m_texture, 3, 128, 128, 128, 128);
+		break;
+	case ZomebieType::skinny:
+		m_animation = Animation(m_texture, 4, 0, 128 * 2, 128, 128);
 		break;
 	}
 
