328fb505371e3b43bfa9d9cc3d5bbeec431dca1e
Nameless <tyrebrown873@gmail.com>
2020-04-23 13:28:05 -0400

    Fat zombie test

--- moving/Enemy.cpp
+++ moving/Enemy.cpp
@@ -1,9 +1,27 @@
 #include "Enemy.h"
+#include <random>
 
 Enemy::Enemy()
 {
 	m_texture.loadFromFile("Textures/zombies.png");
-	m_animation = Animation(m_texture, 4, 0, 0, 128, 128);
+
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> m_distribution(0, 4);
+
+	//m_type = (ZomebieType)m_distribution(rng);
+	m_type = ZomebieType::fat;
+
+	switch (m_type)
+	{
+	case ZomebieType::handWalker:
+		m_animation = Animation(m_texture, 4, 0, 0, 128, 128);
+		break;
+	case ZomebieType::fat:
+		m_animation = Animation(m_texture, 3, 128, 128, 128, 128);
+		break;
+	}
+
 	m_sprite.setTexture(m_texture);
 	m_sprite.setPosition(50.f, 50.f);
 }
--- moving/Enemy.h
+++ moving/Enemy.h
@@ -11,9 +11,15 @@ public:
 	void Update(float dt);
 	void Render(sf::RenderWindow& window);
 private:
+	enum class ZomebieType
+	{
+		handWalker, fat, skinny, oneManJand   
+	};
+private:
 	sf::Sprite m_sprite;
 	sf::Texture m_texture;
 
+	ZomebieType m_type;
 	Animation m_animation;
 };
 
